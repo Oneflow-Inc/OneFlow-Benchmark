9180e22e63ae1a4dc296192243dc2e2e1682478b
ShawnXuan <xiexuanx2@gmail.com>
2019-09-18 18:32:19 +0800

    remove some default value

--- bert_benchmark/run_pretraining.py
+++ bert_benchmark/run_pretraining.py
@@ -9,18 +9,15 @@ from datetime import datetime
 import oneflow as flow
 from pretrain import PreTrain#, Eval
 
-_DATA_DIR = '/dataset/bert/of_wiki_seq_len_128'
-_MODEL_LOAD = "/dataset/model_zoo/bert_new_snapshot/of_L-12_H-768_A-12_random_init"
 _MODEL_SAVE_DIR = "./model_save-{}".format(
     str(datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))
 )
-NODE_LIST = "192.168.1.15,192.168.1.16"
 parser = argparse.ArgumentParser(description="flags for bert")
 
 # resouce
 parser.add_argument("--gpu_num_per_node", type=int, default=1)
 parser.add_argument("--node_num", type=int, default=1)
-parser.add_argument("--node_list", type=str, default=NODE_LIST)
+parser.add_argument("--node_list", type=str, default=None)
 
 # train
 parser.add_argument("--learning_rate", type=float, default=1e-4, help="Learning rate")
@@ -28,9 +25,9 @@ parser.add_argument("--weight_l2", type=float, default=0.01, help="weight l2 dec
 parser.add_argument("--batch_size_per_device", type=int, default=24)
 parser.add_argument("--iter_num", type=int, default=10, help="total iterations to run")
 parser.add_argument("--log_every_n_iter", type=int, default=1, help="print loss every n iteration")
-parser.add_argument("--data_dir", type=str, default=_DATA_DIR)
+parser.add_argument("--data_dir", type=str, default=None)
 parser.add_argument("--data_part_num", type=int, default=32, help="data part number in dataset")
-parser.add_argument("--model_load_dir", type=str, default=_MODEL_LOAD)
+parser.add_argument("--model_load_dir", type=str, default=None)
 parser.add_argument("--model_save_dir", type=str, default=_MODEL_SAVE_DIR)
 
 # bert
