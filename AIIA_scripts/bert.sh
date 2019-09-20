ee854b9557e39fcc7b27a61bfa9c0081de617cd0
mir-of <mir-of@outlook.com>
2019-09-20 17:59:02 +0800

    add aiia scripts

---
+++ AIIA_scripts/bert.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+GPU_NUM_PER_NODE=${1}
+NODE_NUM=${2}
+NODE_LIST=${3}
+RUN_REAL_DATA=${4}
+RUN_SYNTHETIC_DATA=${5}
+LOGFILE=${6}
+
+CMD="python ../bert_benchmark/run_pretraining.py \
+--gpu_num_per_node=$GPU_NUM_PER_NODE \
+--node_num=$NODE_NUM \
+--node_list=$NODE_LIST \
+--learning_rate=1e-4 \
+--weight_l2=0.01 \
+--batch_size_per_device=24 \
+--iter_num=5 \
+--loss_print_every_n_iter=1 \
+--data_part_num=1 \
+--seq_length=128 \
+--max_predictions_per_seq=20 \
+--num_hidden_layers=12 \
+--num_attention_heads=12 \
+--max_position_embeddings=512 \
+--type_vocab_size=2 \
+--vocab_size=30522 \
+--attention_probs_dropout_prob=0.1 \
+--hidden_dropout_prob=0.1 \
+--hidden_size_per_head=64 \
+--data_dir=/dataset/bert/of_wiki_seq_len_128"
+
+
+#set -x
+    (
+     $CMD
+    ) |& tee $LOGFILE
+
+#set +x
---
+++ AIIA_scripts/vgg16.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+GPU_NUM_PER_NODE=${1}
+NODE_NUM=${2}
+NODE_LIST=${3}
+RUN_REAL_DATA=${4}
+RUN_SYNTHETIC_DATA=${5}
+LOGFILE=${6}
+
+CMD="python ../cnn_benchmark/of_cnn_benchmarks.py \
+--model=vgg16 \
+--gpu_num_per_node=$GPU_NUM_PER_NODE \
+--node_num=$NODE_NUM \
+--node_list=$NODE_LIST \
+--batch_size_per_device=8 \
+--iter_num=5 \
+--learning_rate=0.01 \
+--optimizer=sgd \
+--loss_print_every_n_iter=1 \
+--warmup_iter_num=2"
+
+# synthetic data
+
+
+# real data
+if [ $RUN_REAL_DATA = "True" ] ; then
+CMD+=" --data_dir=/dataset/PNGS/PNG228/of_record_repeated"
+#set -x
+    (
+     $CMD
+    ) |& tee $LOGFILE".real"
+
+#set +x
+fi
