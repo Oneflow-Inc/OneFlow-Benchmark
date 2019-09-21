879ea772b09afb50f7815c3670ea804bc9398e57
mir-of <mir-of@outlook.com>
2019-09-21 15:05:06 +0800

    add extract result to json file

--- AIIA_scripts/run_benchmark.py
+++ AIIA_scripts/run_benchmark.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import glob
+import json
 import argparse
 
 parser = argparse.ArgumentParser(description="flags for cnn benchmark")
@@ -16,6 +18,17 @@ parser.add_argument("--output_dir", type=str, default="./output/benchmark_log", 
 args = parser.parse_args()
 
 
+class AutoVivification(dict):
+  """Implementation of perl's autovivification feature."""
+
+  def __getitem__(self, item):
+    try:
+      return dict.__getitem__(self, item)
+    except KeyError:
+      value = self[item] = type(self)()
+    return value
+
+
 def run_model(model, gpu_per_node=1, node_num=1, node_list=None, run_real_data=True, run_synthetic_data=True):
   log_dir = os.path.join(args.output_dir, model)
   if not os.path.exists(log_dir):
@@ -27,7 +40,46 @@ def run_model(model, gpu_per_node=1, node_num=1, node_list=None, run_real_data=T
   print("Saving log to: {}".format(log_path))
 
 
-def main():
+def extract_result():
+  result_dict = AutoVivification()
+
+  logs_list = glob.glob(os.path.join(args.output_dir, "*/*.log*"))
+  for l in logs_list:
+    # extract info from file name
+    model = l.strip().split('/')[-2]  # eg: vgg16
+    file_name = l.strip().split('/')[-1].split('.')
+    run_case = file_name[0]  # eg: 1n1c
+    data_case = file_name[-1] if len(file_name) == 3 else None  # eg: real or synthetic
+
+    # extract info from file content
+    tmp_dict = {}
+    with open(l) as f:
+      lines =  f.readlines()
+      tmp_dict['time_stamp'] = lines[0].strip().split(' ')[0] + '-' + lines[0].strip().split(' ')[1]
+
+      for line in lines:
+        if "batch_size_per_device" in line:
+          tmp_dict['batch_size'] = int(line.strip().split('=')[-1].strip())
+        if "average speed" in line:
+          tmp_dict['average_speed'] = float(line.strip().split(':')[-1].strip().split('(')[0])
+
+
+    if data_case:
+      result_dict[model][run_case][data_case]['time_stamp'] = tmp_dict['time_stamp']
+      result_dict[model][run_case][data_case]['average_speed'] = tmp_dict['average_speed']
+      result_dict[model][run_case][data_case]['batch_size'] = tmp_dict['batch_size']
+    else:
+      result_dict[model][run_case]['time_stamp'] = tmp_dict['time_stamp']
+      result_dict[model][run_case]['average_speed'] = tmp_dict['average_speed']
+      result_dict[model][run_case]['batch_size'] = tmp_dict['batch_size']
+
+  # write to file as JSON format
+  result_file_name = os.path.join(args.output_dir, "oneflow_benchmark_result.json")
+  with open(result_file_name, 'w') as f:
+    json.dump(result_dict, f)
+
+
+def benchmark():
   model_list = args.model.strip().split(',')
   assert len(model_list) > 0
 
@@ -47,6 +99,11 @@ def main():
 
       run_model(model, gpu_per_node=gpu_per_node, node_num=node_num, node_list=args.node_list,
                 run_real_data=args.run_real_data, run_synthetic_data=args.run_real_data)
+
+
+def main():
+  benchmark()
+  extract_result()
 
 
 if __name__ == "__main__":
