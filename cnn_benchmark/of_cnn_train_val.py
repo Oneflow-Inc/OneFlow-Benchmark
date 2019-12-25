7e4b164e9816ac50e063c8a03e8981a1bebf3b05
ShawnXuan <xiexuanx2@gmail.com>
2019-12-25 14:39:31 +0800

    train validation

---
+++ cnn_benchmark/of_cnn_train_val.py
@@ -0,0 +1,252 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import argparse
+import time
+import numpy as np
+from datetime import datetime
+
+import oneflow as flow
+
+import data_loader
+import vgg_model
+import resnet_model
+import alexnet_model
+import benchmark_util
+from util import Snapshot, Summary, print_args, make_lr
+
+
+parser = argparse.ArgumentParser(description="flags for cnn benchmark")
+
+# resouce
+parser.add_argument("--gpu_num_per_node", type=int, default=1)
+parser.add_argument("--node_num", type=int, default=1)
+parser.add_argument("--node_list", type=str, default=None, help="nodes' IP address, split by comma")
+
+parser.add_argument("--model", type=str, default="vgg16", help="vgg16 or resnet50")
+
+# train
+parser.add_argument("--model_load_dir", type=str, default=None, help="model load directory if need")
+parser.add_argument("--batch_size_per_device", type=int, default=8)
+parser.add_argument("--learning_rate", type=float, default=1e-4)
+parser.add_argument("--optimizer", type=str, default="sgd", help="sgd, adam, momentum")
+parser.add_argument("--weight_l2", type=float, default=None, help="weight decay parameter")
+parser.add_argument("--train_step_num", type=int, default=10, help="total training step number")
+parser.add_argument("--data_dir", type=str, default=None, help="training dataset directory")
+parser.add_argument("--data_part_num", type=int, default=32, help="training data part number")
+parser.add_argument("--image_size", type=int, default=228, help="image size")
+## snapshot
+parser.add_argument(
+    "--model_save_every_n_iter",
+    type=int,
+    default=200,
+    help="save model every n iteration",
+)
+parser.add_argument(
+    "--model_save_dir",
+    type=str,
+    default="./output/model_save-{}".format(str(datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))),
+    help="model save directory",
+)
+
+# validation
+parser.add_argument("--val_step_num", type=int, default=10, help="total validation step number")
+parser.add_argument("--val_batch_size_per_device", type=int, default=8)
+parser.add_argument("--val_data_dir", type=str, default=None, help="validation dataset directory")
+parser.add_argument("--val_data_part_num", type=int, default=32, help="validation data part number")
+
+# log and loss print
+parser.add_argument("--log_dir", type=str, default="./output", help="log info save directory")
+parser.add_argument(
+    "--loss_print_every_n_iter",
+    type=int,
+    default=1,
+    help="print loss every n iteration",
+)
+parser.add_argument(
+    "--val_print_every_n_iter",
+    type=int,
+    default=10,
+    help="print loss every n iteration",
+)
+
+args = parser.parse_args()
+summary = Summary(args.log_dir, args)
+
+
+model_dict = {
+    "resnet50": resnet_model.resnet50,
+    "vgg16": vgg_model.vgg16,
+    "alexnet": alexnet_model.alexnet,
+}
+
+optimizer_dict = {
+    "sgd": {"naive_conf": {}},
+    "adam": {"adam_conf": {"beta1": 0.9}},
+    "momentum": {"momentum_conf": {"beta": 0.9}},
+    "momentum-decay": {
+        "momentum_conf": {"beta": 0.9},
+        "learning_rate_decay": {
+            "polynomial_conf": {"decay_batches": 300000, "end_learning_rate": 0.0001,},
+        },
+    },
+}
+
+#        "warmup_conf": {"linear_conf": {"warmup_batches":10000, "start_multiplier":0}},
+
+
+flow.config.gpu_device_num(args.gpu_num_per_node)
+def get_train_config():
+    train_config = flow.function_config()
+    train_config.default_distribute_strategy(flow.distribute.consistent_strategy())
+    train_config.default_data_type(flow.float)
+    train_config.train.primary_lr(args.learning_rate)
+    train_config.disable_all_reduce_sequence(True)
+    # train_config.all_reduce_lazy_ratio(0)
+
+    # train_config.enable_nccl_hierarchical_all_reduce(True)
+    # train_config.cudnn_buf_limit_mbyte(2048)
+    # train_config.concurrency_width(2)
+    train_config.all_reduce_group_num(128)
+    train_config.all_reduce_group_min_mbyte(8)
+
+    train_config.train.model_update_conf(optimizer_dict[args.optimizer])
+
+    if args.weight_l2:
+        train_config.train.weight_l2(args.weight_l2)
+
+    # train_config.enable_inplace(False)
+    # train_config.ctrl_port(12140)
+    return train_config
+
+
+@flow.function(get_train_config())
+def TrainNet():
+    total_device_num = args.node_num * args.gpu_num_per_node
+    batch_size = total_device_num * args.batch_size_per_device
+
+    if args.data_dir:
+        assert os.path.exists(args.data_dir)
+        print("Loading data from {}".format(args.data_dir))
+        (labels, images) = data_loader.load_imagenet(
+            args.data_dir, args.image_size, batch_size, args.data_part_num)
+    else:
+        print("Loading synthetic data.")
+        (labels, images) = data_loader.load_synthetic(args.image_size, batch_size)
+
+    logits = model_dict[args.model](images)
+    loss = flow.nn.sparse_softmax_cross_entropy_with_logits(labels, logits, name="softmax_loss")
+    flow.losses.add_loss(loss)
+    outputs = {"loss": loss}
+
+    #train_config = flow.function_config()
+    #print(dir(train_config.train))
+    #print(type(train_config.train))
+    #step_lr = make_lr("System-Train-TrainStep-TrainNet",
+    #    train_config.train.model_update_conf(),
+    #    train_config.train.primary_lr(),
+    #    train_config.train.secondary_lr())
+    #outputs.update(step_lr)
+
+    return outputs
+
+
+def get_val_config():
+    val_config = flow.function_config()
+    val_config.default_distribute_strategy(flow.distribute.consistent_strategy())
+    val_config.default_data_type(flow.float)
+    return val_config
+
+
+@flow.function(get_val_config())
+def InferenceNet():
+    total_device_num = args.node_num * args.gpu_num_per_node
+    batch_size = total_device_num * args.val_batch_size_per_device
+
+    if args.val_data_dir:
+        assert os.path.exists(args.val_data_dir)
+        print("Loading data from {}".format(args.val_data_dir))
+        # TODO: use different image preprocess
+        (labels, images) = data_loader.load_imagenet(
+            args.val_data_dir, args.image_size, batch_size, args.val_data_part_num)
+    else:
+        print("Loading synthetic data.")
+        (labels, images) = data_loader.load_synthetic(
+            args.image_size, batch_size)
+
+    logits = model_dict[args.model](images)
+    softmax = flow.nn.softmax(logits)
+    return (softmax, labels)
+
+
+def main():
+    print_args(args)
+    def train_callback(step):
+        def callback(train_outputs):
+            loss = train_outputs['loss'].mean()
+            summary.scalar('loss', loss, step)
+            #summary.scalar('learning_rate', train_outputs['lr'], step)
+            if (step-1) % args.loss_print_every_n_iter == 0:
+                print("iter {}, loss: {:.6f}".format(step-1, loss))
+        return callback
+
+    def do_predictions(step, predict_step, predictions):
+        classfications = np.argmax(predictions[0], axis=1)
+        labels = predictions[1]
+        if predict_step == 0:
+            main.correct = 0.0
+            main.total = 0.0
+        else:
+            main.correct += np.sum(classfications == labels);
+            main.total += len(labels)
+        if predict_step + 1 == args.val_step_num:
+            assert main.total > 0
+            summary.scalar('top1_accuracy', main.correct/main.total, step)
+            #summary.scalar('top1_correct', main.correct, step)
+            #summary.scalar('total_val_images', main.total, step)
+            print("iter {}, top 1 accuracy: {:.6f}".format(step, main.correct/main.total))
+
+    def predict_callback(step, predict_step):
+        def callback(predictions):
+            do_predictions(step, predict_step, predictions)
+        return callback
+
+    flow.env.grpc_use_no_signal()
+    flow.env.log_dir(args.log_dir)
+
+    if args.node_num > 1:
+        nodes = []
+        for n in args.node_list.strip().split(","):
+            addr_dict = {}
+            addr_dict["addr"] = n
+            nodes.append(addr_dict)
+
+        flow.env.machine(nodes)
+
+    snapshot = Snapshot(args.model_save_dir, args.model_load_dir)
+
+    total_batch_size = (args.node_num * args.gpu_num_per_node * args.batch_size_per_device)
+    speedometer = benchmark_util.CNNSpeedometer()
+
+    for step in range(args.train_step_num):
+        # save model every n iter
+        if step % args.model_save_every_n_iter == 0:
+            # do validation when save model
+            if step >= 0: # >=0 will trigger validation at step 0
+                for predict_step in range(args.val_step_num):
+                    InferenceNet().async_get(predict_callback(step, predict_step))
+            snapshot.save(step)
+
+        TrainNet().async_get(train_callback(step+1))
+
+    step += 1
+    for predict_step in range(args.val_step_num):
+        do_predictions(step, predict_step, InferenceNet().get()) #use sync mode
+    snapshot.save(step)
+    summary.save()
+
+
+if __name__ == "__main__":
+    main()
---
+++ cnn_benchmark/util.py
@@ -0,0 +1,91 @@
+import os
+import time
+import pandas as pd
+from datetime import datetime
+import oneflow as flow
+
+
+class Snapshot:
+    def __init__(self, model_save_dir, model_load_dir):
+        self._model_save_dir = model_save_dir
+        self._check_point = flow.train.CheckPoint()
+        if model_load_dir:
+            assert os.path.isdir(model_load_dir)
+            print("Restoring model from {}.".format(model_load_dir))
+            self._check_point.load(model_load_dir)
+        else:
+            print("Init model on demand.")
+            self._check_point.init()
+
+    def save(self, step):
+        snapshot_save_path = os.path.join(self._model_save_dir, "snapshot_%d" % step)
+        if not os.path.exists(snapshot_save_path):
+            os.makedirs(snapshot_save_path)
+        print("Saving model to {}.".format(snapshot_save_path))
+        self._check_point.save(snapshot_save_path)
+
+class Summary():
+    def __init__(self, log_dir, config):
+        self._log_dir = log_dir
+        self._metrics = pd.DataFrame({"iter": 0, "legend": "cfg", "note": str(config)}, index=[0])
+
+    def scalar(self, legend, value, step=-1):
+        # TODO: support rank(which device/gpu)
+        df = pd.DataFrame(
+            {"iter": step, "legend": legend, "value": value, "rank": 0, "time": time.time()},
+            index=[0])
+        self._metrics = pd.concat([self._metrics, df], axis=0, sort=False)
+
+    def save(self):
+        save_path = os.path.join(self._log_dir, "summary.csv")
+        self._metrics.to_csv(save_path, index=False)
+        print("saved: {}".format(save_path))
+
+
+def make_lr(train_step_name, model_update_conf, primary_lr, secondary_lr=None):
+    # usually, train_step_name is "System-Train-TrainStep-" + train job name
+    assert model_update_conf.HasField("learning_rate_decay") or model_update_conf.HasField("warmup_conf"), "only support model update conf with warmup or lr decay for now"
+    flow.config.train.train_step_lbn(train_step_name + "-Identity" + "/out")
+    secondary_lr_lbn = "System-Train-SecondaryLearningRate-Scheduler/out"
+    if secondary_lr is None:
+        secondary_lr_lbn = "System-Train-PrimaryLearningRate-Scheduler/out"
+    flow.config.train.lr_lbn("System-Train-PrimaryLearningRate-Scheduler/out",
+                             "System-Train-SecondaryLearningRate-Scheduler/out")
+    # these two lines above must be called before creating any op
+    with flow.device_prior_placement("cpu", "0:0"):
+        train_step = flow.get_variable(
+            name=train_step_name,
+            shape=(1,),
+            dtype=flow.int64,
+            initializer=flow.constant_initializer(0),
+            trainable=False
+        )
+        train_step_id = flow.identity(train_step, name=train_step_name + "-Identity")
+        flow.assign(train_step, train_step_id + 1, name=train_step_name + "-Assign")
+
+        primary_lr_blob = flow.schedule(train_step_id, model_update_conf, primary_lr,
+            name="System-Train-PrimaryLearningRate-Scheduler")
+        secondary_lr_blob = None
+        if secondary_lr is None:
+            secondary_lr_blob = primary_lr_blob
+        else:
+            secondary_lr_blob = flow.schedule(train_step_id, model_update_conf, secondary_lr,
+                name="System-Train-SecondaryLearningRate-Scheduler")
+        assert secondary_lr_blob is not None
+
+        return {
+            "train_step": train_step_id,
+            "lr": primary_lr_blob,
+            "lr2": secondary_lr_blob
+        }
+
+def print_args(args):
+    print("=".ljust(66, "="))
+    print("Running {}: num_gpu_per_node = {}, num_nodes = {}.".format(
+            args.model, args.gpu_num_per_node, args.node_num))
+    print("=".ljust(66, "="))
+    for arg in vars(args):
+        print("{} = {}".format(arg, getattr(args, arg)))
+    print("-".ljust(66, "-"))
+    print("Time stamp: {}".format(str(datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))))
+
