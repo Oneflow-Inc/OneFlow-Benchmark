1f53172cad9759cb94767c2c5c626ba37c043fa9
ShawnXuan <xiexuanx2@gmail.com>
2020-02-08 11:03:36 +0800

    fix

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ rm -rf core.*
 #DATA_ROOT=/mnt/13_nfs/xuan/ImageNet
 DATA_ROOT=/dataset/imagenet-mxnet
 python cnn_benchmark/dali.py \
-  --data_train=$DATA_ROOT/mxnet/train.rec \
-  --data_train_idx=$DATA_ROOT/mxnet/train.idx \
-  --data_val=$DATA_ROOT/mxnet/val.rec \
-  --data_val_idx=$DATA_ROOT/mxnet/val.idx \
+  --data_train=$DATA_ROOT/train.rec \
+  --data_train_idx=$DATA_ROOT/train.idx \
+  --data_val=$DATA_ROOT/val.rec \
+  --data_val_idx=$DATA_ROOT/val.idx \
   --val_batch_size_per_device=20 \
   --gpu_num_per_node=4 \
   --num_examples=1024 \
