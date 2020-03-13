4b7801348b2e5322d5938877a605ebc5fb2d8b24
ShawnXuan <xiexuanx2@gmail.com>
2020-03-13 20:14:28 +0800

    modify run scripts for of

--- of_e2e.sh
+++ of_e2e.sh
@@ -1,19 +1,19 @@
 rm -rf core.* 
-DATA_ROOT=/mnt/13_nfs/xuan/ImageNet/mxnet
+DATA_ROOT=/dataset/ImageNet/ofrecord
 #DATA_ROOT=/dataset/imagenet-mxnet
   #python3 cnn_benchmark/of_cnn_train_val.py \
 #gdb --args \
 #nvprof -f -o resnet.nvvp \
-  python3 cnn_e2e/dali_cnn_train_val.py \
-    --data_train=$DATA_ROOT/train.rec \
-    --data_train_idx=$DATA_ROOT/train.idx \
-    --data_val=$DATA_ROOT/val.rec \
-    --data_val_idx=$DATA_ROOT/val.idx \
+  python3 cnn_e2e/of_cnn_train_val.py \
+    --train_data_dir=$DATA_ROOT/train \
+    --train_data_part_num=256 \
+    --val_data_dir=$DATA_ROOT/validation \
+    --val_data_part_num=256 \
     --gpu_num_per_node=4 \
     --optimizer="momentum-cosine-decay" \
     --learning_rate=0.256 \
     --loss_print_every_n_iter=20 \
-    --batch_size_per_device=64 \
+    --batch_size_per_device=32 \
     --val_batch_size_per_device=125 \
     --model="resnet50" 
     #--use_fp16 true \
