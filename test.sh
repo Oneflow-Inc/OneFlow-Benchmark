2efc131d169adc723d386e7919727a1d661bed8b
ShawnXuan <xiexuanx2@gmail.com>
2020-03-25 15:56:24 +0800

    modify test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,19 @@
 rm -rf core.*
 #gdb --args \
-#DATA_ROOT=/mnt/13_nfs/xuan/ImageNet
-DATA_ROOT=/mnt/13_nfs/xuan/ImageNet/mxnet
-#DATA_ROOT=/dataset/imagenet-mxnet
-python cnn_benchmark/dali_consistent.py \
-  --data_train=$DATA_ROOT/train.rec \
-  --data_train_idx=$DATA_ROOT/train.idx \
-  --data_val=$DATA_ROOT/val.rec \
-  --data_val_idx=$DATA_ROOT/val.idx \
-  --val_batch_size_per_device=20 \
-  --loss_print_every_n_iter=200 \
-  --gpu_num_per_node=4 \
-  --num_epochs=90
-  #--num_examples=1024 \
+#DATA_ROOT=/dataset/imagenet_10pics
+DATA_ROOT=/dataset/imagenet_1pic
+python cnn_e2e/test_dali.py \
+  --data_train=$DATA_ROOT/mxnet/train.rec \
+  --data_train_idx=$DATA_ROOT/mxnet/train.idx \
+  --data_val=$DATA_ROOT/mxnet/train.rec \
+  --data_val_idx=$DATA_ROOT/mxnet/train.idx \
+  --train_data_dir=$DATA_ROOT/ofrecord \
+  --train_data_part_num=1 \
+  --val_data_dir=$DATA_ROOT/ofrecord \
+  --val_data_part_num=1 \
+  --val_batch_size_per_device=1 \
+  --batch_size_per_device=1 \
+  --num_examples=1 \
+  --num_val_examples=1 \
+  --gpu_num_per_node=1 \
+  --num_epochs=1
