daed129efaf047f82a16f676ef307d51d55ce80e
ShawnXuan <xiexuanx2@gmail.com>
2020-05-16 15:46:36 +0800

    work with cc's new dataloader

--- of_e2e.sh
+++ of_e2e.sh
@@ -4,14 +4,14 @@ rm -rf ./output/snapshots/*
 DATA_ROOT=/dataset/ImageNet/ofrecord
 #DATA_ROOT=/dataset/imagenet-mxnet
   #python3 cnn_benchmark/of_cnn_train_val.py \
-#gdb --args \
 #nvprof -f -o resnet.nvvp \
+#gdb --args \
   python3 cnn_e2e/of_cnn_train_val.py \
     --train_data_dir=$DATA_ROOT/train \
     --train_data_part_num=256 \
     --val_data_dir=$DATA_ROOT/validation \
     --val_data_part_num=256 \
-    --num_nodes=2 \
+    --num_nodes=1 \
     --node_ips='11.11.1.12,11.11.1.14' \
     --gpu_num_per_node=4 \
     --optimizer="momentum-cosine-decay" \
