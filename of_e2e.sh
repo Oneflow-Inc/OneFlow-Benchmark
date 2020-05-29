04a16945a803f64b333d6f19d5be3e6e19382a7c
ShawnXuan <xiexuanx2@gmail.com>
2020-05-29 15:11:37 +0800

    modify for local test

--- of_e2e.sh
+++ of_e2e.sh
@@ -1,8 +1,8 @@
 rm -rf core.* 
 rm -rf ./output/snapshots/*
 #DATA_ROOT=/DATA/disk1/of_imagenet_example
-DATA_ROOT=/DATA/disk1/ImageNet/ofrecord
-#DATA_ROOT=/dataset/ImageNet/ofrecord
+#DATA_ROOT=/DATA/disk1/ImageNet/ofrecord
+DATA_ROOT=/dataset/ImageNet/ofrecord
 #DATA_ROOT=/dataset/imagenet-mxnet
   #python3 cnn_benchmark/of_cnn_train_val.py \
 #nvprof -f -o resnet.nvvp \
@@ -14,11 +14,11 @@ DATA_ROOT=/DATA/disk1/ImageNet/ofrecord
     --val_data_part_num=256 \
     --num_nodes=1 \
     --node_ips='11.11.1.12,11.11.1.14' \
-    --gpu_num_per_node=8 \
+    --gpu_num_per_node=4 \
     --optimizer="momentum-cosine-decay" \
     --learning_rate=0.256 \
     --loss_print_every_n_iter=20 \
-    --batch_size_per_device=64 \
+    --batch_size_per_device=56 \
     --val_batch_size_per_device=125 \
     --use_boxing_v2=True \
     --use_new_dataloader=True \
