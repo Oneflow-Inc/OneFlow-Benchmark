07e85ab7cdace884c4a974512af1d4702e8d48bd
ShawnXuan <xiexuanx2@gmail.com>
2020-06-26 00:50:53 +0800

    add weight decay

--- bert_benchmark/run_pretraining.py
+++ bert_benchmark/run_pretraining.py
@@ -29,10 +29,10 @@ parser.add_argument("--node_list", type=str, default=None)
 
 # train
 parser.add_argument("--learning_rate", type=float, default=1e-4, help="Learning rate")
-parser.add_argument("--weight_l2", type=float, default=0.01, help="weight l2 decay parameter")
-parser.add_argument("--batch_size_per_device", type=int, default=24)
-parser.add_argument("--iter_num", type=int, default=10, help="total iterations to run")
-parser.add_argument("--warmup_iter_num", type=int, default=10, help="total iterations to run")
+parser.add_argument("--weight_decay_rate", type=float, default=0.01, help="weight decay rate")
+parser.add_argument("--batch_size_per_device", type=int, default=64)
+parser.add_argument("--iter_num", type=int, default=1144000, help="total iterations to run")
+parser.add_argument("--warmup_batches", type=int, default=10000)
 parser.add_argument("--log_every_n_iter", type=int, default=1, help="print loss every n iteration")
 parser.add_argument("--data_dir", type=str, default=None)
 parser.add_argument("--data_part_num", type=int, default=32, help="data part number in dataset")
@@ -140,12 +140,20 @@ def BuildPreTrainNet(
 
 _BERT_MODEL_UPDATE_CONF = dict(
     learning_rate_decay=dict(
-        polynomial_conf=dict(decay_batches=1000000, end_learning_rate=0.0,)
+        polynomial_conf=dict(
+            decay_batches=args.iter_num,
+            end_learning_rate=0.0,
+        )
     ),
-    warmup_conf=dict(linear_conf=dict(
-        warmup_batches=10000, start_multiplier=0,)),
+    warmup_conf=dict(
+        linear_conf=dict(warmup_batches=args.warmup_batches, start_multiplier=0,)
+    ),
     clip_conf=dict(clip_by_global_norm=dict(clip_norm=1.0,)),
     adam_conf=dict(epsilon=1e-6),
+    weight_decay_conf=dict(
+        weight_decay_rate=args.weight_decay_rate,
+        excludes=dict(pattern=["bias", "LayerNorm", "layer_norm"]),
+    ),
 )
 
 config = flow.function_config()
@@ -153,7 +161,6 @@ config.default_data_type(flow.float)
 config.default_distribute_strategy(flow.distribute.consistent_strategy())
 config.train.primary_lr(args.learning_rate)
 config.train.model_update_conf(_BERT_MODEL_UPDATE_CONF)
-# config.train.weight_l2(args.weight_l2) ??
 
 if args.use_fp16:
     config.enable_auto_mixed_precision(True)
