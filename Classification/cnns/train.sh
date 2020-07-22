0d23bd26e4b97d787894cebebbe6ef901aca4d0d
Flowingsun007 <Flowingsun007@163.com>
2020-07-22 22:41:24 +0800

    change flow.math.relu to flow.nn.relu

--- Classification/cnns/train.sh
+++ Classification/cnns/train.sh
@@ -2,8 +2,7 @@ rm -rf core.*
 rm -rf ./output/snapshots/*
 
 # Set path to your dataset root
-# DATA_ROOT=/dataset/ImageNet/ofrecord
-DATA_ROOT=dataset
+DATA_ROOT=/dataset/ImageNet/ofrecord
 
 python3 of_cnn_train_val.py \
     --train_data_dir=$DATA_ROOT/train \
@@ -13,11 +12,11 @@ python3 of_cnn_train_val.py \
     --val_data_part_num=256 \
     --num_val_examples=50000  \
     --num_nodes=1 \
-    --gpu_num_per_node=1 \
+    --gpu_num_per_node=4 \
     --model_update="momentum" \
     --learning_rate=0.256 \
     --loss_print_every_n_iter=10 \
     --batch_size_per_device=64 \
-    --val_batch_size_per_device=100 \
+    --val_batch_size_per_device=50 \
     --num_epoch=90 \
     --model="resnet50"
