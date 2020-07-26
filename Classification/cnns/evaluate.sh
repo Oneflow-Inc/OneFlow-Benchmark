44f7deacc41c3b408c333344d8c5359f6dd33acc
Flowingsun007 <Flowingsun007@163.com>
2020-07-26 23:28:07 +0800

    add code and script for cnn's evaluation

---
+++ Classification/cnns/evaluate.sh
@@ -0,0 +1,19 @@
+rm -rf core.* 
+
+# Set up  dataset root dir
+DATA_ROOT=/dataset/ImageNet/ofrecord
+
+# Set up model path, e.g. :  vgg16_of_best_model_val_top1_721  alexnet_of_best_model_val_top1_54762 
+MODEL_LOAD_DIR="resnet_v15_of_best_model_val_top1_77318"
+
+  python3  of_cnn_evaluate.py \
+    --num_epochs=1 \
+    --num_val_examples=50000 \
+    --model_load_dir=$MODEL_LOAD_DIR  \
+    --val_data_dir=$DATA_ROOT/validation \
+    --val_data_part_num=256 \
+    --num_nodes=1 \
+    --node_ips='127.0.0.1' \
+    --gpu_num_per_node=4 \
+    --val_batch_size_per_device=32 \
+    --model="resnet50"
