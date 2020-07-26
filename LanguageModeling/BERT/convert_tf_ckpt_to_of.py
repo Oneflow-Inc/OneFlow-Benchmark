c8489cf2acac5ddeff03dd8e5008da5881958ecf
ShawnXuan <xiexuanx2@gmail.com>
2020-07-26 19:32:20 +0800

    convert ckpt support extra weights

--- LanguageModeling/BERT/convert_tf_ckpt_to_of.py
+++ LanguageModeling/BERT/convert_tf_ckpt_to_of.py
@@ -23,15 +23,33 @@ parser.add_argument("--of_dump_path",
                     required = True,
                     help = "Path to the output OneFlow model.")
 
-args = parser.parse_args()
+#args = parser.parse_args()
+args, unknown = parser.parse_known_args()
+print(args)
 
-def _SaveWeightBlob2File(blob, folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+# parse unknown arguments for extra weights
+extra_weights = {}
+for u in unknown:
+    w = u.split("=")
+    assert len(w) == 2
+    if len(w) == 2:
+        extra_weights[w[0]] = float(w[1])
+
+
+def _write_blob(folder, blob):
+    os.makedirs(folder, exist_ok=True)
     filename = os.path.join(folder, "out")
     f = open(filename, 'wb')
     f.write(blob.tobytes())
     f.close()
+    print(filename, blob.shape)
+
+def _SaveWeightBlob2File(blob, folder):
+    _write_blob(folder, blob)
+
+    for weight, default_value in extra_weights.items():
+        d = np.full_like(blob, default_value)
+        _write_blob(folder + weight, d)
 
 def convert():
     path = args.tf_checkpoint_path
@@ -50,7 +68,7 @@ def convert():
 
         folder_name = op_name+"-"+blob_name
         folder = os.path.join(args.of_dump_path, folder_name)
-        print("saved to:", folder)
+        #print("saved to:", folder)
 
         _SaveWeightBlob2File(array, folder)
 
