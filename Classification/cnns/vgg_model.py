5ed5de830c04737b3fb7db61c902a9dcbe72993f
ShawnXuan <xiexuanx2@gmail.com>
2020-08-08 20:54:56 +0800

    rm usless import

--- Classification/cnns/vgg_model.py
+++ Classification/cnns/vgg_model.py
@@ -15,7 +15,6 @@ limitations under the License.
 """
 
 import oneflow as flow
-import oneflow.core.operator.op_conf_pb2 as op_conf_util
 
 def _batch_norm(inputs, name=None, trainable=True):
     return flow.layers.batch_normalization(
