a349f799e8e4147ea6ef14341252daf160bf1611
Flowingsun007 <Flowingsun007@163.com>
2020-08-31 11:02:37 +0800

    replace warning function >> flow.image.Resize()  with new api

--- Classification/cnns/train.sh
+++ Classification/cnns/train.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 rm -rf core.* 
 rm -rf ./output/snapshots/*
 
@@ -15,6 +15,29 @@ python3 of_cnn_train_val.py \
     --val_batch_size_per_device=16 \
     --num_epoch=10 \
     --model="resnet50"
+
+
+
+# # training with mini-imagenet
+# DATA_ROOT=data/mini-imagenet/ofrecord
+# python3 of_cnn_train_val.py \
+#     --train_data_dir=$DATA_ROOT/train \
+#     --num_examples=50 \
+#     --train_data_part_num=1 \
+#     --val_data_dir=$DATA_ROOT/validation \
+#     --num_val_examples=50 \
+#     --val_data_part_num=1 \
+#     --num_nodes=1 \
+#     --gpu_num_per_node=1 \
+#     --model_update="momentum" \
+#     --learning_rate=0.001 \
+#     --loss_print_every_n_iter=1 \
+#     --batch_size_per_device=16 \
+#     --val_batch_size_per_device=10 \
+#     --num_epoch=10 \
+#     --model="resnet50"
+
+
 
 
 # # training with imagenet
