6a3a483351d4d353a60db15ff81dc7cddea38c4b
Flowingsun007 <Flowingsun007@163.com>
2020-09-07 08:21:56 +0800

    default to training synthetic data

--- Classification/cnns/train.sh
+++ Classification/cnns/train.sh
@@ -2,20 +2,20 @@
 rm -rf core.* 
 rm -rf ./output/snapshots/*
 
-# # training with synthetic data
-# python3 of_cnn_train_val.py \
-#     --num_examples=50 \
-#     --num_val_examples=50 \
-#     --num_nodes=1 \
-#     --gpu_num_per_node=1 \
-#     --optimizer="sgd" \
-#     --momentum=0.875 \
-#     --learning_rate=0.001 \
-#     --loss_print_every_n_iter=1 \
-#     --batch_size_per_device=16 \
-#     --val_batch_size_per_device=16 \
-#     --num_epoch=10 \
-#     --model="resnet50"
+# training with synthetic data
+python3 of_cnn_train_val.py \
+    --num_examples=50 \
+    --num_val_examples=50 \
+    --num_nodes=1 \
+    --gpu_num_per_node=1 \
+    --optimizer="sgd" \
+    --momentum=0.875 \
+    --learning_rate=0.001 \
+    --loss_print_every_n_iter=1 \
+    --batch_size_per_device=16 \
+    --val_batch_size_per_device=16 \
+    --num_epoch=10 \
+    --model="resnet50"
 
 
 
@@ -42,29 +42,28 @@ rm -rf ./output/snapshots/*
 
 
 
-# training with imagenet
- #DATA_ROOT=/DATA/disk1/ImageNet/ofrecord
- DATA_ROOT=/datasets/ImageNet/ofrecord
- LOG_FOLDER=../logs
- mkdir -p $LOG_FOLDER
- LOGFILE=$LOG_FOLDER/resnet_training.log
+# # training with imagenet
+#  DATA_ROOT=/datasets/ImageNet/ofrecord
+#  LOG_FOLDER=../logs
+#  mkdir -p $LOG_FOLDER
+#  LOGFILE=$LOG_FOLDER/resnet_training.log
 
-python3 of_cnn_train_val_new.py \
-     --train_data_dir=$DATA_ROOT/train \
-     --train_data_part_num=256 \
-     --val_data_dir=$DATA_ROOT/validation \
-     --val_data_part_num=256 \
-     --num_nodes=1 \
-     --gpu_num_per_node=8 \
-     --optimizer="sgd" \
-     --momentum=0.875 \
-     --label_smoothing=0.1 \
-     --learning_rate=1.024 \
-     --loss_print_every_n_iter=100 \
-     --batch_size_per_device=128 \
-     --val_batch_size_per_device=100 \
-     --num_epoch=90 \
-     --model="resnet50" 2>&1 | tee ${LOGFILE}
+# python3 of_cnn_train_val_new.py \
+#      --train_data_dir=$DATA_ROOT/train \
+#      --train_data_part_num=256 \
+#      --val_data_dir=$DATA_ROOT/validation \
+#      --val_data_part_num=256 \
+#      --num_nodes=1 \
+#      --gpu_num_per_node=8 \
+#      --optimizer="sgd" \
+#      --momentum=0.875 \
+#      --label_smoothing=0.1 \
+#      --learning_rate=1.024 \
+#      --loss_print_every_n_iter=100 \
+#      --batch_size_per_device=128 \
+#      --val_batch_size_per_device=100 \
+#      --num_epoch=90 \
+#      --model="resnet50" 2>&1 | tee ${LOGFILE}
 
-echo "Writting log to ${LOGFILE}" 
+# echo "Writting log to ${LOGFILE}" 
 
