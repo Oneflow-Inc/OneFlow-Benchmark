e1839b1781dd07e439099852a458ba86b75514f2
Flowingsun007 <Flowingsun007@163.com>
2020-09-09 17:51:24 +0800

    recovery evaluate.sh

--- Classification/cnns/evaluate.sh
+++ Classification/cnns/evaluate.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
 rm -rf core.* 
 
 # Set up  dataset root dir
-DATA_ROOT=/datasets/ImageNet/ofrecord
+DATA_ROOT=/dataset/ImageNet/ofrecord
 
 # Set up model path, e.g. :  vgg16_of_best_model_val_top1_721  alexnet_of_best_model_val_top1_54762 
 MODEL_LOAD_DIR="resnet_v15_of_best_model_val_top1_77318"
@@ -18,21 +17,3 @@ MODEL_LOAD_DIR="resnet_v15_of_best_model_val_top1_77318"
     --gpu_num_per_node=4 \
     --val_batch_size_per_device=64 \
     --model="resnet50"
-
-
-
-
-# # Evaluate with mini-imagenet
-# DATA_ROOT=data/mini-imagenet/ofrecord
-# MODEL_LOAD_DIR="output/snapshots/model_save-20200907130848/snapshot_epoch_9"
-# python3  of_cnn_evaluate.py \
-#     --num_epochs=3 \
-#     --num_val_examples=50 \
-#     --model_load_dir=$MODEL_LOAD_DIR  \
-#     --val_data_dir=$DATA_ROOT/validation \
-#     --val_data_part_num=1 \
-#     --num_nodes=1 \
-#     --node_ips='127.0.0.1' \
-#     --gpu_num_per_node=1 \
-#     --val_batch_size_per_device=10 \
-#     --model="resnet50"
