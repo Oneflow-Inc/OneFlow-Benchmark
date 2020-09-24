87486ebeee1a05a18364fa13ac772eb3764b6dd6
ouyangyu <xuanjiuye@gmail.com>
2020-09-24 14:53:51 +0800

    add benchmark.sh

--- Classification/cnns/train.sh
+++ Classification/cnns/train.sh
@@ -1,39 +1,40 @@
-BENCH_ROOT=$1
-NUM_NODES=$2
-GPU_NUM_PER_NODE=$3
-BSZ_PER_DEVICE=$4
-DATA_ROOT=$5
-#DATA_ROOT=/dataset/ImageNet/ofrecord
-# DATA_ROOT=/datasets/ImageNet/OneFlow
-DATA_PART_NUM=44
-rm -rf ./log
-mkdir ./log
-NUM_ITERS=300
-NUM_EXAMPLES=$(($NUM_NODES * $GPU_NUM_PER_NODE * $BSZ_PER_DEVICE * $NUM_ITERS))
-export PYTHONUNBUFFERED=1
-echo PYTHONUNBUFFERED=$PYTHONUNBUFFERED
-export NCCL_LAUNCH_MODE=PARALLEL
-echo NCCL_LAUNCH_MODE=$NCCL_LAUNCH_MODE
-python3 $BENCH_ROOT/of_cnn_train_val.py \
-    --num_examples=$NUM_EXAMPLES \
-    --train_data_dir=$DATA_ROOT/train \
-    --train_data_part_num=$DATA_PART_NUM \
-    --num_nodes=$NUM_NODES \
-    --gpu_num_per_node=$GPU_NUM_PER_NODE \
-    --optimizer="sgd" \
-    --momentum=0.875 \
-    --label_smoothing=0.1 \
-    --learning_rate=0.001 \
-    --loss_print_every_n_iter=100 \
-    --batch_size_per_device=$BSZ_PER_DEVICE \
-    --val_batch_size_per_device=125 \
-    --num_epoch=1 \
-    --log_dir=./log \
-    --use_fp16 \
-    --channel_last=True \
-    --pad_output \
-    --fuse_bn_relu=True \
-    --fuse_bn_add_relu=True \
-    --nccl_fusion_threshold_mb=16 \
-    --nccl_fusion_max_ops=24 \
-    --model="resnet50"
+rm -rf core.*
+rm -rf ./output/snapshots/*
+
+if [ -n "$1" ]; then
+    NUM_EPOCH=$1
+else
+    NUM_EPOCH=50
+fi
+echo NUM_EPOCH=$NUM_EPOCH
+
+# training with imagenet
+if [ -n "$2" ]; then
+    DATA_ROOT=$2
+else
+    DATA_ROOT=/data/imagenet/ofrecord
+fi
+echo DATA_ROOT=$DATA_ROOT
+
+LOG_FOLDER=../logs
+mkdir -p $LOG_FOLDER
+LOGFILE=$LOG_FOLDER/resnet_training.log
+
+python3 of_cnn_train_val.py \
+     --train_data_dir=$DATA_ROOT/train \
+     --train_data_part_num=256 \
+     --val_data_dir=$DATA_ROOT/validation \
+     --val_data_part_num=256 \
+     --num_nodes=1 \
+     --gpu_num_per_node=8 \
+     --optimizer="sgd" \
+     --momentum=0.875 \
+     --label_smoothing=0.1 \
+     --learning_rate=1.024 \
+     --loss_print_every_n_iter=100 \
+     --batch_size_per_device=128 \
+     --val_batch_size_per_device=50 \
+     --num_epoch=$NUM_EPOCH \
+     --model="resnet50" 2>&1 | tee ${LOGFILE}
+
+echo "Writting log to ${LOGFILE}"
