e63a12db825384f5453080d69b1ad82da17a59cf
ouyangyu <xuanjiuye@gmail.com>
2020-10-06 08:33:29 +0800

    reset train.sh

--- Classification/cnns/train.sh
+++ Classification/cnns/train.sh
@@ -20,11 +20,6 @@ LOG_FOLDER=../logs
 mkdir -p $LOG_FOLDER
 LOGFILE=$LOG_FOLDER/resnet_training.log
 
-export PYTHONUNBUFFERED=1
-echo PYTHONUNBUFFERED=$PYTHONUNBUFFERED
-export NCCL_LAUNCH_MODE=PARALLEL
-echo NCCL_LAUNCH_MODE=$NCCL_LAUNCH_MODE
-
 python3 of_cnn_train_val.py \
      --train_data_dir=$DATA_ROOT/train \
      --train_data_part_num=256 \
@@ -35,15 +30,10 @@ python3 of_cnn_train_val.py \
      --optimizer="sgd" \
      --momentum=0.875 \
      --label_smoothing=0.1 \
-     --learning_rate=0.968 \
+     --learning_rate=1.024 \
      --loss_print_every_n_iter=100 \
-     --batch_size_per_device=64 \
+     --batch_size_per_device=128 \
      --val_batch_size_per_device=50 \
-     --channel_last=False \
-     --fuse_bn_relu=True \
-     --fuse_bn_add_relu=True \
-     --nccl_fusion_threshold_mb=16 \
-     --nccl_fusion_max_ops=24 \
      --num_epoch=$NUM_EPOCH \
      --model="resnet50" 2>&1 | tee ${LOGFILE}
 
