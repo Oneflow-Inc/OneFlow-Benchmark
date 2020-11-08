76e39b59ef03470a0e536a81d373d07b331005a5
ShawnXuan <xiexuanx2@gmail.com>
2020-11-08 23:03:10 +0800

    optimizer

--- LanguageModeling/gpt-2/src/util.py
+++ LanguageModeling/gpt-2/src/util.py
@@ -2,13 +2,15 @@ import os
 import oneflow as flow
 
 def CreateOptimizer(args):
-    #warmup_batches = int(args.iter_num * args.warmup_proportion)
-    #lr_warmup = flow.optimizer.warmup.linear(warmup_batches, 0)
-    lr_scheduler = flow.optimizer.PolynomialSchduler(args.learning_rate, 100000, 0.0)
-                                                     #warmup=lr_warmup)
-    return flow.optimizer.AdamW(lr_scheduler, epsilon=1e-6, weight_decay=1e-3, #args.weight_decay_rate,
-                                weight_decay_excludes=["bias", "LayerNorm", "layer_norm"],
-                                grad_clipping=flow.optimizer.grad_clipping.by_global_norm(1.0))
+    lr_scheduler = flow.optimizer.PiecewiseConstantScheduler([], [args.learning_rate])
+    if args.optimizer == 'adam':
+        opt = flow.optimizer.Adam(lr_scheduler, do_bias_correction=True) 
+    elif args.optimizer == 'sgd':
+        opt = flow.optimizer.SGD(lr_scheduler, momentum=0.0)
+    else:
+        exit('Bad optimizer:', args.optimizer)
+    return opt
+
 
 def GetFunctionConfig(args):
     config = flow.function_config()
