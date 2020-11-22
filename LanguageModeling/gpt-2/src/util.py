15a74f0948befcee19dd44575557ad31c5b1d038
ShawnXuan <xiexuanx2@gmail.com>
2020-11-22 16:25:21 +0800

    support metric

--- LanguageModeling/gpt-2/src/util.py
+++ LanguageModeling/gpt-2/src/util.py
@@ -1,10 +1,12 @@
 import os
+import time
+from collections import OrderedDict
 import oneflow as flow
 
 def CreateOptimizer(args):
     lr_scheduler = flow.optimizer.PiecewiseConstantScheduler([], [args.learning_rate])
     if args.optimizer == 'adam':
-        opt = flow.optimizer.Adam(lr_scheduler, do_bias_correction=True) 
+        opt = flow.optimizer.Adam(lr_scheduler, do_bias_correction=True)
     elif args.optimizer == 'sgd':
         opt = flow.optimizer.SGD(lr_scheduler, momentum=0.0)
     else:
@@ -45,7 +47,7 @@ class Snapshot(object):
             self._check_point.load(model_load_dir)
         else:
             self._check_point.init()
-            self.save('initial_model')
+            #self.save('initial_model')
             print("Init model on demand.")
 
     def save(self, name):
@@ -54,3 +56,83 @@ class Snapshot(object):
             os.makedirs(snapshot_save_path)
         print("Saving model to {}.".format(snapshot_save_path))
         self._check_point.save(snapshot_save_path)
+
+
+class StopWatch(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        self.start_time = time.time()
+        self.last_split = self.start_time
+
+    def split(self):
+        now = time.time()
+        duration = now - self.last_split
+        self.last_split = now
+        return duration
+
+    def stop(self):
+        self.stop_time = time.time()
+
+    def duration(self):
+        return self.stop_time - self.start_time
+
+
+class Metric(object):
+    def __init__(self, desc='train', print_steps=-1, batch_size=256, keys=[]):
+        r"""accumulate and calculate metric
+
+        Args:
+            desc: `str` general description of the metric to show
+            print_steps: `Int` print metrics every nth steps
+            batch_size: `Int` batch size per step
+            keys: keys in callback outputs
+        Returns:
+        """
+        self.desc = desc
+        self.print_steps = print_steps
+        assert batch_size > 0
+        self.batch_size = batch_size
+
+        assert isinstance(keys, (list, tuple))
+        self.keys = keys
+        self.metric_dict = OrderedDict()
+        self.metric_dict['step'] = 0
+
+        self.timer = StopWatch()
+        self.timer.start()
+        self._clear()
+
+    def _clear(self):
+        for key in self.keys:
+            self.metric_dict[key] = 0.0
+        self.metric_dict['throughput'] = 0.0
+        self.num_samples = 0.0
+
+    def update_and_save(self, key, value, step, **kwargs):
+        self.metric_dict[key] = value
+
+    def metric_cb(self, step=0, **kwargs):
+        def callback(outputs):
+            if step == 0: self._clear()
+
+            for key in self.keys:
+                self.metric_dict[key] += outputs[key].sum()
+
+            self.num_samples += self.batch_size
+
+            if (step + 1) % self.print_steps == 0:
+                self.metric_dict['step'] = step + 1
+                for k, v in kwargs.items():
+                    self.metric_dict[k] = v
+                throughput = self.num_samples / self.timer.split()
+                self.update_and_save('throughput', throughput, step)
+                for key in self.keys:
+                    value = self.metric_dict[key] / self.num_samples
+                    self.update_and_save(key, value, step, **kwargs)
+                print(', '.join(('{}: {}' if type(v) is int else '{}: {:.3f}').format(k, v) \
+                                for k, v in self.metric_dict.items()), time.time())
+                self._clear()
+
+        return callback
