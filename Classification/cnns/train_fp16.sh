0caac3a97dbedc0bd72eef867941e111ccc637f8
ouyangyu <xuanjiuye@gmail.com>
2020-11-23 12:11:21 +0800

    remove cnn adam in sh

--- Classification/cnns/train_fp16.sh
+++ Classification/cnns/train_fp16.sh
@@ -32,9 +32,7 @@ python3 of_cnn_train_val.py \
      --val_data_part_num=256 \
      --num_nodes=1 \
      --gpu_num_per_node=8 \
-     --optimizer="adam" \
-     --epsilon=1e-6 \
-     --gradient_clipping=1.0 \
+     --optimizer="sgd" \
      --momentum=0.875 \
      --label_smoothing=0.1 \
      --learning_rate=1.536 \
