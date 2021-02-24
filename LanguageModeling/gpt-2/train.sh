e2b49745b0dfd1eb7b34f32baa366c57a2dc6319
Flowingsun007 <flowingsun007@163.com>
2021-02-24 15:22:04 +0800

    recovery to origin

--- LanguageModeling/gpt-2/train.sh
+++ LanguageModeling/gpt-2/train.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=1
 
-script_path=$(realpath $0)
-gpt_root=$(dirname $script_path)
-data_path=$gpt_root/data/wiki_00
-cfg_path=$gpt_root/models/117M
+gpt2_dir=gpt-2
+gpt2_path=/OneFlow-Benchmark/LanguageModeling/$gpt2_dir
+model_path=/wksp/gpt2_small_of_models_4_regression
+data_path=$model_path/data/wiki_00
+cfg_path=$model_path/117M
+model_load_path=$model_path/of_models_new
 
-gpu_num_per_node=8
-batch_size=8
+gpu_num_per_node=1
+batch_size=1
 
 n_vocab=50257
 n_ctx=1024
@@ -22,13 +24,16 @@ log_dir=log
 test_case=bsz${batch_size}_g${gpu_num_per_node}_e${n_embd}_h${n_head}_l${n_layer}
 log_file=$log_dir/$test_case.log
 
+set -x
 
 mkdir -p $log_dir
+ln -s $gpt2_path
 
-python3 -m src.train \
+python3 -m $gpt2_dir.src.train \
   --dataset=$data_path \
   --cfg_dir=$cfg_path \
-  --save_last_snapshot=False \
+  --model_load_dir=$model_load_path \
+  --save_last_snapshot=True \
   --iter_num=100 \
   --loss_print_every_n_iter=1 \
   --total_batch_size=$batch_size \
