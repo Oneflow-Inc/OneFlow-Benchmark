983a38cb461564b2c9c41309978199e46f6b4154
ouyangyu <xuanjiuye@gmail.com>
2021-03-09 18:53:34 +0800

    add bert script

--- LanguageModeling/BERT/util.py
+++ LanguageModeling/BERT/util.py
@@ -147,7 +147,14 @@ def CreateOptimizer(args):
     loss_scale_policy = None
     if args.use_fp16:
         loss_scale_policy = flow.optimizer.loss_scale.dynamic_loss_scale(increment_period=2000);
-    return flow.optimizer.AdamW(lr_scheduler, epsilon=1e-6, weight_decay=args.weight_decay_rate,
+
+    if args.optimizer_type == "lamb":
+        return flow.optimizer.LAMB(lr_scheduler, beta1=0.9, beta2=0.999, epsilon=1e-6, weight_decay=args.weight_decay_rate,
+                                weight_decay_excludes=["bias", "LayerNorm", "layer_norm"],
+                                grad_clipping=flow.optimizer.grad_clipping.by_global_norm(1.0),
+                                loss_scale_policy=loss_scale_policy)
+    else:
+        return flow.optimizer.AdamW(lr_scheduler, epsilon=1e-6, weight_decay=args.weight_decay_rate,
                                 weight_decay_excludes=["bias", "LayerNorm", "layer_norm"],
                                 grad_clipping=flow.optimizer.grad_clipping.by_global_norm(1.0),
                                 loss_scale_policy=loss_scale_policy)
