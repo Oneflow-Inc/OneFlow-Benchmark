f14690e18cb4d5872350bb014d1844bc642a194d
Flowingsun007 <flowingsun007@163.com>
2021-05-03 09:01:41 +0800

    update test scripts

---
+++ LanguageModeling/gpt-2/run_single_node_test.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+current_dir=$(dirname $(readlink -f "$0"))
+model=${1:-gpt2-small}
+batch_size_per_device=${2:-8}
+zero_stage=${3:-0}
+checkpoint_activations=${4:-"off"}
+dtype=${5:-'fp16'}
+test_num=${6:-5}
+
+
+i=1
+while [ $i -le ${test_num} ]
+do
+    bash $current_dir/test_zero_optimization.sh $model $batch_size_per_device 1 1  $checkpoint_activations $zero_stage $dtype ${i}
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Finished Test Case ${i}!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
+    let i++
+    sleep 20s
+done
+
+i=1
+while [ $i -le ${test_num} ]
+do
+    bash $current_dir/test_zero_optimization.sh $model $batch_size_per_device 1 4  $checkpoint_activations $zero_stage $dtype ${i}
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Finished Test Case ${i}!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
+    let i++
+    sleep 20s
+done
+
+i=1
+while [ $i -le ${test_num} ]
+do
+    bash $current_dir/test_zero_optimization.sh $model $batch_size_per_device 1 8  $checkpoint_activations $zero_stage $dtype ${i}
+    echo " >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Finished Test Case ${i}!<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< "
+    let i++
+    sleep 20s
+done
