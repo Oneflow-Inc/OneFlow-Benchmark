3f6c2f648bef958705793fe324c00566fca4f90b
leaves-zwx <kunta0932@gmail.com>
2021-05-12 16:35:28 +0800

    update examples

--- LanguageModeling/GPT/examples/pretrain_with_container.sh
+++ LanguageModeling/GPT/examples/pretrain_with_container.sh
@@ -18,6 +18,6 @@ export ONEFLOW_GTP_PRETRAIN_WITH_CONTAINER=ON
 export ONEFLOW_GPT_SRC_DIR=$(realpath $(dirname $(dirname $0)))
 export ONEFLOW_DEV_IMAGE=oneflow-manylinux2014-cuda11.2:0.1
 export ONEFLOW_GPT_PYTHON_VERSION=3.7
-export ONEFLOW_WHEEL=$PWD/packages/oneflow-0.3.5+cu112.git.4a4f032-cp37-cp37m-linux_x86_64.whl
+export ONEFLOW_WHEEL=$PWD/packages/oneflow-0.3.5+cu102.git.8b222eed2-cp37-cp37m-manylinux2014_x86_64.whl
 
 source $(dirname $0)/pretrain.sh
