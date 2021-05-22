f8d384ab062047b9c3e12b5bd80a71fa4d2d652f
Ldpe2G <liangdepeng@gmail.com>
2021-05-23 00:50:51 +0800

    update test_record script

--- Classification/cnn_modules/test_record.py
+++ Classification/cnn_modules/test_record.py
@@ -34,17 +34,6 @@ crop_mirror_norm = flow.nn.CropMirrorNormalize(color_space=color_space, output_l
 with flow.no_grad():
     rng = flip()
 
-# val_record_reader = flow.nn.OfrecordReader("/home/ldpe2g/oneFlow/oneflowBenckmark/dataset/imagenette/ofrecord/val",
-#                                           batch_size=batch_size,
-#                                           data_part_num=1,
-#                                           part_name_suffix_length=5,
-#                                           shuffle_after_epoch=False)
-# val_record_image_decoder = flow.nn.OFRecordImageDecoder("encoded", color_space=color_space)
-# val_resize = flow.nn.image.Resize(resize_side="shorter", keep_aspect_ratio=True, target_size=256)
-# val_crop_mirror_normal = flow.nn.CropMirrorNormalize(color_space=color_space, output_layout=output_layout,
-#                                                     crop_h=height, crop_w=width, crop_pos_y=0.5, crop_pos_x=0.5,
-#                                                     mean=rgb_mean, std=rgb_std, output_dtype=flow.float)
-
 train_set_size = 9469
 val_set_size = 3925
 train_loop = train_set_size // batch_size
@@ -54,12 +43,11 @@ val_loop = val_set_size // batch_size
 for i in range(1000000000000000000000):
     print(i)
     start = time.time()
-    with flow.no_grad():
-        train_record = train_record_reader()
-        label = record_label_decoder(train_record)
-        image_raw_buffer = record_image_decoder(train_record)
-        # image = resize(image_raw_buffer)
-        # image = crop_mirror_norm(image, rng)
+    train_record = train_record_reader()
+    label = record_label_decoder(train_record)
+    image_raw_buffer = record_image_decoder(train_record)
+    image = resize(image_raw_buffer)
+    image = crop_mirror_norm(image, rng)
     # print(image.shape, time.time() - start)
 
     # recover images
@@ -72,24 +60,3 @@ for i in range(1000000000000000000000):
     # print(image_np.shape)
     # cv2.imwrite("recover_image%d.jpg" % i, image_np)
 
-# validation
-# for i in range(val_loop):
-#     print(i)
-#     start = time.time()
-#     with flow.no_grad():
-#         val_record = val_record_reader()
-#         label = record_label_decoder(val_record)
-#         image_raw_buffer = val_record_image_decoder(val_record)
-#         image = val_resize(image_raw_buffer)
-#         image = val_crop_mirror_normal(image)
-#     print(image.shape, time.time() - start)
-
-    # # recover images
-    # image_np = image.numpy()
-    # image_np = np.squeeze(image_np)
-    # image_np = np.transpose(image_np, (1, 2, 0))
-    # image_np = image_np * rgb_std + rgb_mean
-    # image_np = cv2.cvtColor(np.float32(image_np), cv2.COLOR_RGB2BGR)
-    # image_np = image_np.astype(np.uint8)
-    # print(image_np.shape)
-    # cv2.imwrite("recover_val_image%d.jpg" % i, image_np)
