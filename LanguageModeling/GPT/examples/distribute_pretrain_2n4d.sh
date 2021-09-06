69606ce7090dd9c69f98e3b45346dd54956a98b2
Ldpe2G <liangdepeng@gmail.com>
2021-09-06 16:34:50 +0800

    update script for 2n4d

--- LanguageModeling/GPT/examples/distribute_pretrain_2n4d.sh
+++ LanguageModeling/GPT/examples/distribute_pretrain_2n4d.sh
@@ -4,10 +4,13 @@
 export ONEFLOW_GPT_NUM_GPUS_PER_NODE=4
 export ONEFLOW_GPT_NUM_NODES=2
 # Set this env for your training nodes ip
-# export ONEFLOW_GPT_NODE_IPS="192.168.1.16,192.168.1.15"
+export ONEFLOW_GPT_NODE_IPS="10.10.120.201,10.10.120.202"
+
+export NCCL_SOCKET_IFNAME=enp49s0f0
+export NCCL_DEBUG=INFO 
 
 # If you place training data on somewhere else, set this env
-# export ONEFLOW_GPT_DATASET=/data/gpt/gpt_sample_dataset_text_document
+export ONEFLOW_GPT_DATASET=/public/data/gpt_sample_dataset_text_document
 export ONEFLOW_GPT_SEQ_LENGTH=2048
 
 export ONEFLOW_GPT_HIDDEN_SIZE=1536
