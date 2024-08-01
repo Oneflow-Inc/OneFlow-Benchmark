6ac0f4cefe6764feba617e135e332b0e0a924e6c
ShawnXuan <xiexuanx2@gmail.com>
2024-08-01 02:56:43 +0000

    rm old files

--- Classification/resnet50/ansible_workspace/update_tools.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "/data/home/chende/tools"
-ansible hosts -i inventory.ini -m copy -a "src=/data/home/chende/tools dest=/data/home/chende/ mode=0755"
--- Classification/resnet50/tools/profile.sh
+++
@@ -1,7 +0,0 @@
-NUM_NODES=${1:-1}
-
-if [ "$NODE_RANK" -lt "$NUM_NODES" ]; then
-  bash examples/args_train_ddp_graph_resnet50.sh "$NUM_NODES" 8 "$NODE_RANK" 192.168.1.27 /data/dataset/ImageNet/ofrecord 128 50 true python3 graph gpu 100 false '/usr/local/cuda/bin/nsys' 1
-else
-  echo do nothing
-fi
