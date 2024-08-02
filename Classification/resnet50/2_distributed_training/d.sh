8911664fcc424eddb6fcc0368f1748afdf2b4f14
ShawnXuan <xiexuanx2@gmail.com>
2024-08-02 01:05:49 +0000

    update

--- Classification/resnet50/2_distributed_training/d.sh
+++
@@ -1,1 +0,0 @@
-ansible-playbook -i ../inventory.ini dist_training.yml
---
+++ Classification/resnet50/2_distributed_training/run_dist_training.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKER_IMAGE="oneflowinc/oneflow:0.9.1.dev20240203-cuda11.8"
+SRC="/share_nfs/k85/models/Vision/classification/image/resnet50"
+
+if [ -n "$1" ]; then
+  DOCKER_IMAGE="$1"
+fi
+
+if [ -n "$2" ]; then
+  SRC="$2"
+fi
+
+# 运行 ansible-playbook 命令
+ansible-playbook -i ../inventory.ini dist_training.yml -e "docker_image=${DOCKER_IMAGE}" -e "src=${SRC}"
